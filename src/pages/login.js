83fd625a7e32e18f3d28a3c65b282b32775a5c09
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-03 17:05:22 +0900

    feat: loginPage 구현

---
+++ src/pages/login.js
@@ -0,0 +1,13 @@
+const Login = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-amber-200">
+        <main className="max-w-lg p-4 bg-white rounded-lg cursor-pointer shadow-">
+          <h1>Login Page</h1>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Login;
