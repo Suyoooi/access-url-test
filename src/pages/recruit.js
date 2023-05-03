8dcda59ff149cae26c6a65271bf4dc2b8cd527b0
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-03 14:50:45 +0900

    feat: recruitPage 추가

---
+++ src/pages/recruit.js
@@ -0,0 +1,13 @@
+const Recruit = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-red-200">
+        <main className="max-w-lg p-4 bg-white rounded-lg cursor-pointer shadow-">
+          <h1>recruit Page</h1>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Recruit;
