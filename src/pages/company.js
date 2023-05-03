35a30f38ce7f65f5b1bfc75966fff19447c268c7
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-03 14:49:55 +0900

    feat: companyPage 생성

---
+++ src/pages/company.js
@@ -0,0 +1,13 @@
+const Company = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center h-screen bg-lime-200">
+        <main className="max-w-lg p-4 bg-white rounded-lg shadow-lg cursor-pointer">
+          <h1>Company Page</h1>
+        </main>
+      </div>
+    </>
+  );
+};
+
+export default Company;
