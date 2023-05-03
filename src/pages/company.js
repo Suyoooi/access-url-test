b13bb421b47babaff5ad4b80b96aa7160f033996
Suyoooi <131094271+Suyoooi@users.noreply.github.com>
2023-05-03 17:05:11 +0900

    feat: companyPage 접근 못하도록 구현

--- src/pages/company.js
+++ src/pages/company.js
@@ -1,4 +1,20 @@
-const Company = () => {
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+export default function Company(props) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // 이전 페이지 확인
+    if (
+      !router.asPath.startsWith("/company") ||
+      !props.referrer ||
+      !props.referrer.startsWith("https://localhost:3000")
+    ) {
+      router.push("/");
+    }
+  }, []);
+
   return (
     <>
       <div className="flex items-center justify-center h-screen bg-lime-200">
@@ -8,6 +24,27 @@ const Company = () => {
       </div>
     </>
   );
-};
+}
 
-export default Company;
+// export async function getServerSideProps(context) {
+//   // 인증 정보 확인
+//   const isLoggedIn = true; // 로그인 여부 체크
+
+//   if (!isLoggedIn) {
+//     return {
+//       redirect: {
+//         destination: "/login",
+//         permanent: false,
+//       },
+//     };
+//   }
+
+//   // 이전 페이지 정보
+//   const { req } = context;
+//   const referrer = req.headers.referer || null;
+//   const props = { referrer };
+
+//   return {
+//     props,
+//   };
+// }
